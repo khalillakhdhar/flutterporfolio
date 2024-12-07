570dec813d74aec2f2e88b892edd87c53c2b8788
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-12-07 20:08:32 +0100

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'widgets/drawer_widget.dart';
 
 class HomePage extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
